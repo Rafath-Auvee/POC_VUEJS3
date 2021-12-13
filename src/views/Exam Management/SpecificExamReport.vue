80759bf38c7529a85e3e5ea9c2516a8742dce730
Jisan-mia <jisanmia47@gmail.com>
2021-12-13 12:44:29 +0600

    login/register started

--- src/views/Exam Management/SpecificExamReport.vue
+++ src/views/Exam Management/SpecificExamReport.vue
@@ -1,57 +1,64 @@
 <template>
-  <header>
+  <header class="header">
     <div class="exam__info">
       <h3 class="exam__header">Exam Info</h3>
 
       <div class="header__container">
 
         <div class="item1">
-          <div class="header__img">
+          <div class="exam__img">
             <img src="/images/placeholderImg.svg" alt="">
           </div>
         </div>
 
         <div class="item2">
           <h3>Physics 1s Paper-2</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam excepturi possimus officia porro eius nisi amet. Eaque, neque similique. Ullam eos vero ipsum saepe.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam excepturi possimus officia porro eius ipsum saepe.</p>
           <p>10:30 AM | Sunday, 19/10/2021</p>
         </div>
 
         <div class="item3">
-          <h3>Assigned Student</h3>
-          <div class="exam__info">
-            <div class="group">
-              <p class="label"></p>
-              <p class="value"></p>
-            </div>
-            <div class="group">
-              <p class="label"></p>
-              <p class="value"></p>
-            </div>
-          </div>
-          <div class="mark__info">
-            <div class="group">
-              <p class="label"></p>
-              <p class="value"></p>
-            </div>
-            <div class="group">
-              <p class="label"></p>
-              <p class="value"></p>
-            </div>
-            <div class="group">
-              <p class="label"></p>
-              <p class="value"></p>
+          <div>
+            <h3>Assigned Student</h3>
+            <div class="more__info">
+              <div class="group">
+                <p class="label">Batch</p>
+                <p class="value">HSC2021</p>
+              </div>
+              <div class="group">
+                <p class="label">Exam Pack</p>
+                <p class="value">Elite Exam Mania</p>
+              </div>
             </div>
           </div>
+          
+          <div>
+            <h3>Marking</h3>
+            <div class="more__info mark__info">
+              <div class="group">
+                <p class="label">Total Mark</p>
+                <p class="value">30</p>
+              </div>
+              <div class="group">
+                <p class="label">Per Question Mark</p>
+                <p class="value">1</p>
+              </div>
+              <div class="group">
+                <p class="label">Pass Mark</p>
+                <p class="value">12.5</p>
+              </div>
+            </div>
+          </div>
+          
         </div>
 
         <div class="item4">
           <h3>Your Performance</h3>
 
           <div class="infos">
-            Score: 25/30
-            Timestamp: 10:20 pm
-            Negative Marketing: -5
+            <p>Score: 25/30</p>
+            <p>Timestamp: 10:20 pm</p>
+            <p>Negative Marketing: -5</p>
           </div>
 
           <button class="view__btn">
@@ -95,7 +102,7 @@
         </td>
         <td>
           <span>
-            time_stamp
+            Timestamp
           </span>
         </td>
         <td>
@@ -451,11 +458,13 @@ table {
 
 }
 .header__input{
-  display: flex;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
   justify-content: flex-start;
   align-content: center;
   gap: 0.85rem;
-  margin-bottom: 1rem;
+  margin-bottom: 0.3rem;
 
   input, select {
     border: 1px solid #FF6F00;
@@ -470,10 +479,7 @@ table {
     &::placeholder{
       color: #696969;
     }
-    &:first-child{
-      min-width: 10%;
-      width: 23%;
-    }
+  
   }
   select{
     text-align: left;
@@ -485,4 +491,160 @@ table {
   
   
 }
+
+.header {
+  @include flexVertical;
+  align-items: flex-start;
+  gap: 1.8rem;
+}
+.exam__info {
+  @extend .header;
+  gap: 1rem;
+
+  width: 100%;
+  .exam__header{
+    font-weight: bold;
+    font-size: 1.5rem;
+    line-height: 28px;
+    color: #00A9DC;
+  }
+
+  .header__container {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(230px, 1fr));
+    gap: 1.3rem;
+    width: 100%;
+
+    .exam__img{
+      background: #CFCFCF;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border-radius: 18px;
+      width: 100%;
+      height: 140px;
+      img{
+        max-width: 75px;
+      }
+    }
+
+    .item2{
+      @include flexVertical;
+      align-items: flex-start;
+      justify-content: space-evenly;
+      h3{
+        font-weight: bold;
+        font-size: 1.1rem;
+        line-height: 25px;
+        color: #00A9DC;
+      }
+      p{
+        &:nth-child(odd) {
+          font-size: 14px;
+          line-height: 17px;
+          color: #000000d3;
+          font-weight: 500;
+        
+        }
+        &:nth-child(even){
+          font-size: 12px;
+          line-height: 15px;
+          color: #000000b3;
+          font-weight: 500;
+        }
+      }
+      
+    }
+
+    .item3{
+      @include flexVertical;
+      align-items: flex-start;
+      justify-content: space-between;
+      div h3{
+        font-weight: bold;
+        font-size: 16px;
+        line-height: 18px;
+        color: #00A9DC;
+        margin-bottom: 0.3rem;
+      }
+
+      .more__info{
+        display: flex;
+        align-items: center;
+        gap: 0.3rem;
+
+        .group{
+          @include flexVertical;
+          gap: 0.2rem;
+          .label{
+            font-weight: normal;
+            font-size: 10px;
+            line-height: 11px;
+            letter-spacing: -0.011em;
+            color: #000000;
+          }
+          .value {
+            font-size: 10px;
+            letter-spacing: -0.011em;
+            color: #000000;
+            border: 1px solid #00A9DC;
+            box-sizing: border-box;
+            border-radius: 5px;
+            padding:  0.4rem 0.8rem;
+          }
+        }
+      }
+      .mark__info{
+        .group {
+          .label {
+            font-size: 9px;
+            line-height: 10px;
+          }
+          .value {
+            border-radius: 0;
+            text-align: center;
+            padding: 0.3rem 1.1rem
+          }
+        }
+      }
+    }
+
+    .item4{
+      @include flexVertical;
+      align-items: flex-start;
+      justify-content: space-evenly;
+      h3{
+        font-weight: bold;
+        font-size: 16px;
+        line-height: 17px;
+        letter-spacing: -0.011em;
+        color: #000000;
+      }
+      div.infos p{
+        font-size: 13px;
+        line-height: 15px;
+        letter-spacing: -0.011em;
+        color: #000000c7;
+        font-weight: 500;
+        margin: 0.2rem 0;
+      }
+
+      .view__btn {
+        border: none;
+        outline: none;
+        background: #FF6F00;
+        color: #fff;
+        padding: 7px 10px;
+        cursor: pointer;
+        font-size: 13px;
+        font-weight: 14px;
+      }
+    }
+    
+
+
+  }
+
+
+} 
 </style>
